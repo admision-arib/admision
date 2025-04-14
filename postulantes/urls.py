cedef17035e320857f0ad51722d898a9ad03151f
desarrollo-arib <desarrollo@iestparib.edu.pe>
2025-04-14 17:52:49 -0500

    agregar url list

--- postulantes/urls.py
+++ postulantes/urls.py
@@ -7,7 +7,7 @@ app_name = 'postulantes'
 urlpatterns = [
     path('registrar/', views.registro_postulante, name='registrar_postulante'),
     path('exito/', views.registro_exitoso, name='registro_exitoso'),
-    #path('listar/', views.listar_postulantes, name='listar_postulantes'),
-    #path('generar_pdf/<int:pk>', views.postulante_print, name='generar_pdf' ),
+    path('listarr/', views.listar_postulantes, name='listar_postulantes'),
+    path('generar_pdf/<int:pk>', views.postulante_print, name='generar_pdf' ),
     path('cuota/', views.storage_quota, name='storage_quota')
 ]
